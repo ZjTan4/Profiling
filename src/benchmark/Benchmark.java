22c32129e47e5f75a8c514f05f694a418545b5ce
ZjTan4 <ztan4@ualberta.ca>
2022-02-26 21:54:41 -0700

    switch to Ubuntu to better call coreutils

--- src/benchmark/Benchmark.java
+++ src/benchmark/Benchmark.java
@@ -4,29 +4,27 @@ import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.infra.Blackhole;
 
 import java.io.File;
-import java.lang.annotation.Annotation;
-import java.util.List;
+
 import java.util.concurrent.TimeUnit;
 
 
 @BenchmarkMode(Mode.AverageTime)
-@OutputTimeUnit(TimeUnit.NANOSECONDS)
 @Measurement(iterations = 5)
 @Warmup(iterations = 3)
+@Fork(2)
+@Threads(8)
 public class Benchmark {
+    @OutputTimeUnit(TimeUnit.SECONDS)
     @org.openjdk.jmh.annotations.Benchmark
     public Process runProcessBuilder() throws Exception {
         final ProcessBuilder processBuilder = new ProcessBuilder("java", "-version");
-        processBuilder.redirectErrorStream(true);
-        File log = new File("./java-version.log");
-        processBuilder.redirectOutput(log);
 
         Process process = processBuilder.start();
 
         return process;
     }
 
-    @org.openjdk.jmh.annotations.Benchmark
+    //@org.openjdk.jmh.annotations.Benchmark
     public void runApache(Blackhole blackhole) {
         // do nothing
         blackhole.consume(new Object());
